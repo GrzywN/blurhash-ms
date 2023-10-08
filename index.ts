f392f5ff0d217668aded2610ec0ef6b8da6f5619
GrzywN <starystachu9@gmail.com>
2023-10-08 12:37:22 +0200

    feat: add blurhash route to router

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
+import { blurhashRoute } from "./src/routes/blurhash-route";
 import { homeRoute } from "./src/routes/home-route";
+import { log } from "./src/utils/logger";
+
+const port = process.env.PORT || 3000;
+
+log(`Server is running on port ${port}`);
 
 Bun.serve({
-  fetch(req) {
+  async fetch(req) {
     const url = new URL(req.url);
 
     switch (url.pathname) {
-      case "/": return homeRoute(req);
+      case "/":
+        return homeRoute(req);
+      case "/blurhash":
+        return await blurhashRoute(req);
     }
 
     return new Response(null, { status: 400 });
@@ -17,5 +26,5 @@ Bun.serve({
       },
     });
   },
-  port: process.env.PORT || 3000,
+  port,
 });
