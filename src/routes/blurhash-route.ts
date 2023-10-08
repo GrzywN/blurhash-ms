217fcd50f2642017a25442dad8abc44de8f0bbd2
GrzywN <starystachu9@gmail.com>
2023-10-08 12:35:29 +0200

    feat: add blurhash route

---
+++ src/routes/blurhash-route.ts
@@ -0,0 +1,16 @@
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { post } from "../controllers/blurhash-controller";
+
+async function blurhashRoute(request: Request): Promise<Response> {
+  switch (request.method) {
+    case "POST":
+      return await post(request);
+  }
+
+  return Response.json(
+    { message: ReasonPhrases.METHOD_NOT_ALLOWED },
+    { status: StatusCodes.METHOD_NOT_ALLOWED }
+  );
+}
+
+export { blurhashRoute };
